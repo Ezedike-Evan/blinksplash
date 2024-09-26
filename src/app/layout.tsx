8d406986ae37a27423ec13cc0222015653f1f55d
unknown <egwomevan323@gmail.com>
2024-09-26 15:02:58 +0100

    modified the height of the home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children}: Readonly<{children: React.ReactNode}>){
   return (
     <html lang="en">
+      <head>
+        <style>
+          @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap');
+        </style>
+      </head>
       <body>
         <WalletProviderComponent>
           {children}
